dfeed1adb6fb2d50a16eb9303a76a80f60280968
Katerina Koukiou <kkoukiou@redhat.com>
2018-05-10 14:51:26 +0200

    events: Register VIR_DOMAIN_EVENT_ID_IO_ERROR_REASON
    Signed-off-by: Katerina Koukiou <kkoukiou@redhat.com>

--- src/events.c
+++ src/events.c
@@ -266,6 +266,32 @@ virtDBusEventsDomainIOError(virConnectPtr connection G_GNUC_UNUSED,
 }
 
 static gint
+virtDBusEventsDomainIOErrorReason(virConnectPtr connection G_GNUC_UNUSED,
+                                  virDomainPtr domain,
+                                  const gchar *srcPath,
+                                  const gchar *device,
+                                  gint action,
+                                  const gchar *reason,
+                                  gpointer opaque)
+{
+    virtDBusConnect *connect = opaque;
+    g_autofree gchar *path = NULL;
+
+    path = virtDBusUtilBusPathForVirDomain(domain, connect->domainPath);
+
+    g_dbus_connection_emit_signal(connect->bus,
+                                  NULL,
+                                  path,
+                                  VIRT_DBUS_DOMAIN_INTERFACE,
+                                  "IOErrorReason",
+                                  g_variant_new("(ssus)", srcPath, device,
+                                                action, reason),
+                                  NULL);
+
+    return 0;
+}
+
+static gint
 virtDBusEventsDomainTrayChange(virConnectPtr connection G_GNUC_UNUSED,
                                virDomainPtr domain,
                                const gchar *device,
@@ -499,6 +525,10 @@ virtDBusEventsRegister(virtDBusConnect *connect)
     virtDBusEventsRegisterDomainEvent(connect,
                                       VIR_DOMAIN_EVENT_ID_IO_ERROR,
                                       VIR_DOMAIN_EVENT_CALLBACK(virtDBusEventsDomainIOError));
+
+    virtDBusEventsRegisterDomainEvent(connect,
+                                      VIR_DOMAIN_EVENT_ID_IO_ERROR_REASON,
+                                      VIR_DOMAIN_EVENT_CALLBACK(virtDBusEventsDomainIOErrorReason));
 
     virtDBusEventsRegisterDomainEvent(connect,
                                       VIR_DOMAIN_EVENT_ID_REBOOT,
